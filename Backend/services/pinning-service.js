22f75656ccf62e705b3483ed5b3f8551905db68e
purvas2005 <spurva449@gmail.com>
2026-03-10 17:18:42 +0530

    Fixed the db error situation

--- Backend/services/pinning-service.js
+++ Backend/services/pinning-service.js
@@ -2,7 +2,7 @@
 const { generateCertificate } = require("./certificate");
 const { uploadBufferToPinata, uploadJsonToPinata } = require("./pinata");
 const { mintCertificateNft } = require("./polygon");
-const { saveCertificateRecord } = require("./database_old");
+const { saveCertificateRecord } = require("./database");
 
 /**
  * Orchestrates the entire certificate minting process (QR → FINAL metadata).
